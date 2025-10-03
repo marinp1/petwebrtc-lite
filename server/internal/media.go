bb2a7c04975b6bd57062e1eb113987555c0e5fbb
Patrik Marin <dev@patrikmarin.fi>
2025-10-03 04:27:16 +0000

    Improve client handling

--- server/internal/media.go
+++ server/internal/media.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"math/rand"
 	"sync"
+	"time"
 
 	"github.com/pion/rtp"
 	"github.com/pion/rtp/codecs"
@@ -10,14 +11,21 @@ import (
 )
 
 type Client struct {
-	PeerConn   *webrtc.PeerConnection
-	VideoTrack *webrtc.TrackLocalStaticRTP
-	Packetizer rtp.Packetizer
+	PeerConn      *webrtc.PeerConnection
+	VideoTrack    *webrtc.TrackLocalStaticRTP
+	Packetizer    rtp.Packetizer
+	lastTimestamp uint32
+	startTime     time.Time
+	naluChan      chan []byte
+	done          chan struct{}
 }
 
 type ClientManager struct {
-	Clients map[*Client]struct{}
-	Mu      sync.RWMutex
+	Clients      map[*Client]struct{}
+	Mu           sync.RWMutex
+	lastKeyframe []byte
+	lastSPS      []byte
+	lastPPS      []byte
 }
 
 func NewClientManager() *ClientManager {
@@ -28,21 +36,35 @@ func NewClientManager() *ClientManager {
 
 func (cm *ClientManager) BroadcastNALUs(naluChan <-chan []byte) {
 	for nalu := range naluChan {
-		cm.Mu.RLock()
-		clients := make([]*Client, 0, len(cm.Clients))
-		for c := range cm.Clients {
-			clients = append(clients, c)
-		}
-		cm.Mu.RUnlock()
+		cm.cacheKeyframes(nalu)
 
-		for _, c := range clients {
-			if c.PeerConn.ConnectionState() == webrtc.PeerConnectionStateConnected {
-				packets := c.Packetizer.Packetize(nalu, 90000/30)
-				for _, pkt := range packets {
-					_ = c.VideoTrack.WriteRTP(pkt)
-				}
+		cm.Mu.RLock()
+		for c := range cm.Clients {
+			select {
+			case c.naluChan <- nalu:
+			default:
+				// Client can't keep up, skip frame
 			}
 		}
+		cm.Mu.RUnlock()
+	}
+}
+
+func (cm *ClientManager) cacheKeyframes(nalu []byte) {
+	if len(nalu) < 5 {
+		return
+	}
+	naluType := nalu[4] & 0x1F
+	switch naluType {
+	case 7: // SPS
+		cm.lastSPS = make([]byte, len(nalu))
+		copy(cm.lastSPS, nalu)
+	case 8: // PPS
+		cm.lastPPS = make([]byte, len(nalu))
+		copy(cm.lastPPS, nalu)
+	case 5: // IDR
+		cm.lastKeyframe = make([]byte, len(nalu))
+		copy(cm.lastKeyframe, nalu)
 	}
 }
 
@@ -50,6 +72,47 @@ func (cm *ClientManager) AddClient(client *Client) {
 	cm.Mu.Lock()
 	cm.Clients[client] = struct{}{}
 	cm.Mu.Unlock()
+
+	cm.Clients[client] = struct{}{}
+
+	// Send cached keyframes immediately
+	if cm.lastSPS != nil {
+		packets := client.Packetizer.Packetize(cm.lastSPS, 0)
+		for _, pkt := range packets {
+			_ = client.VideoTrack.WriteRTP(pkt)
+		}
+	}
+	if cm.lastPPS != nil {
+		packets := client.Packetizer.Packetize(cm.lastPPS, 0)
+		for _, pkt := range packets {
+			_ = client.VideoTrack.WriteRTP(pkt)
+		}
+	}
+	if cm.lastKeyframe != nil {
+		packets := client.Packetizer.Packetize(cm.lastKeyframe, 0)
+		for _, pkt := range packets {
+			_ = client.VideoTrack.WriteRTP(pkt)
+		}
+	}
+
+	// Start per-client sender goroutine
+	go func() {
+		for {
+			select {
+			case nalu := <-client.naluChan:
+				if client.PeerConn.ConnectionState() == webrtc.PeerConnectionStateConnected {
+					timestamp := uint32(time.Since(client.startTime).Milliseconds() * 90) // 90kHz clock
+					packets := client.Packetizer.Packetize(nalu, timestamp-client.lastTimestamp)
+					client.lastTimestamp = timestamp
+					for _, pkt := range packets {
+						_ = client.VideoTrack.WriteRTP(pkt)
+					}
+				}
+			case <-client.done:
+				return
+			}
+		}
+	}()
 }
 
 func (cm *ClientManager) RemoveClient(client *Client) {
@@ -64,5 +127,7 @@ func NewClient(pc *webrtc.PeerConnection, track *webrtc.TrackLocalStaticRTP) *Cl
 		1200, 96, ssrc, &codecs.H264Payloader{},
 		rtp.NewRandomSequencer(), 90000,
 	)
-	return &Client{pc, track, packetizer}
+	naluChan := make(chan []byte, 100)
+	done := make(chan struct{})
+	return &Client{pc, track, packetizer, 0, time.Now(), naluChan, done}
 }
